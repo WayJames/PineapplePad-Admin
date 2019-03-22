15d57b254190f4c8b45575893921d6af54194a0d
Daniel Heppner <heppned@wwu.edu>
2019-03-22 13:19:32 -0700

    added navbar button

---
+++ src/components/NavbarUserButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar-menu is-active">
+    <div v-if="!user" class="navbar-item">
+      <a @click="$emit('loginClicked')" class="button is-primary">
+        <span class="icon">
+          <i class="fas fa-user"></i>
+        </span>
+        <span>Login/Sign up</span>
+      </a>
+    </div>
+    <div v-if="!!user" class="navbar-item has-dropdown is-hoverable">
+      <router-link
+        :to="{name: 'dashboard'}"
+        class="navbar-link"
+      >Hey there, {{user.attributes.name}}!</router-link>
+      <div class="navbar-dropdown is-right is-boxed">
+        <a class="navbar-item" @click="$store.dispatch('signOut')">Sign Out</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState([
+    'user'
+  ])
+}
+</script>
